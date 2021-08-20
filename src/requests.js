d26e72a0c0c12c54082ef972ab36f335d1992630
partharoyy <partharoyy88@gmail.com>
2021-08-20 14:03:48 +0530

    Fixed resource

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const API_KEY = "fd7c6479808057c293d83578287ec3cb";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
