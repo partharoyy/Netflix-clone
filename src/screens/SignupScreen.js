13de19694cb23ba210f6ee48f5393ab62f41a8fe
partharoyy <partharoyy88@gmail.com>
2021-08-20 15:24:59 +0530

    fiexd mobile signup form width

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -39,7 +39,7 @@ const SignupScreen = () => {
   };
 
   return (
-    <div className="max-w-[400px] md:max-w-[500px] lg:max-w-[400px] h-[80vh] lg:h-[45vh] md:h-[40vh] p-[20px] md:p-[15px] mr-auto ml-auto bg-signup-screen rounded">
+    <div className="max-w-[450px] md:max-w-[500px] lg:max-w-[400px] h-[80vh] lg:h-[45vh] md:h-[40vh] p-[20px] md:p-[15px] mr-auto ml-auto bg-signup-screen rounded">
       <form className="flex flex-col">
         <h1 className="text-left mb-[30px] lg:text-[1.8rem] lg:mt-[1.5rem] text-[1.8rem] md:text-[2rem] font-semibold">
           Sign In
